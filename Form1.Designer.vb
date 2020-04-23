6b56ab904276f091d19141f43b56ec86e503240d
Taqqazy <58000999+Taqqazy@users.noreply.github.com>
2020-04-24 01:33:22 +0200

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FichierToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OuvrirImageToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.EnregistrerSousToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EnregistrerToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.QuitterToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditerToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OptionsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FichierToolStripMenuItem, Me.EditerToolStripMenuItem, Me.OptionsToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(830, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FichierToolStripMenuItem
+        '
+        Me.FichierToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OuvrirImageToolStripMenuItem, Me.ToolStripSeparator1, Me.EnregistrerSousToolStripMenuItem, Me.EnregistrerToolStripMenuItem, Me.ToolStripSeparator2, Me.QuitterToolStripMenuItem})
+        Me.FichierToolStripMenuItem.Name = "FichierToolStripMenuItem"
+        Me.FichierToolStripMenuItem.Size = New System.Drawing.Size(54, 20)
+        Me.FichierToolStripMenuItem.Text = "Fichier"
+        '
+        'OuvrirImageToolStripMenuItem
+        '
+        Me.OuvrirImageToolStripMenuItem.Name = "OuvrirImageToolStripMenuItem"
+        Me.OuvrirImageToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.OuvrirImageToolStripMenuItem.Text = "Ouvrir image"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(177, 6)
+        '
+        'EnregistrerSousToolStripMenuItem
+        '
+        Me.EnregistrerSousToolStripMenuItem.Name = "EnregistrerSousToolStripMenuItem"
+        Me.EnregistrerSousToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.EnregistrerSousToolStripMenuItem.Text = "Enregistrer sous..."
+        '
+        'EnregistrerToolStripMenuItem
+        '
+        Me.EnregistrerToolStripMenuItem.Name = "EnregistrerToolStripMenuItem"
+        Me.EnregistrerToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.EnregistrerToolStripMenuItem.Text = "Enregistrer"
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(177, 6)
+        '
+        'QuitterToolStripMenuItem
+        '
+        Me.QuitterToolStripMenuItem.Name = "QuitterToolStripMenuItem"
+        Me.QuitterToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.QuitterToolStripMenuItem.Text = "Quitter"
+        '
+        'EditerToolStripMenuItem
+        '
+        Me.EditerToolStripMenuItem.Name = "EditerToolStripMenuItem"
+        Me.EditerToolStripMenuItem.Size = New System.Drawing.Size(49, 20)
+        Me.EditerToolStripMenuItem.Text = "Editer"
+        '
+        'OptionsToolStripMenuItem
+        '
+        Me.OptionsToolStripMenuItem.Name = "OptionsToolStripMenuItem"
+        Me.OptionsToolStripMenuItem.Size = New System.Drawing.Size(61, 20)
+        Me.OptionsToolStripMenuItem.Text = "Options"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.Title = "Ouvrir image"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(191, 63)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(436, 429)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(830, 581)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Head Scanner"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents EditerToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OptionsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents FichierToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OuvrirImageToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents EnregistrerSousToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EnregistrerToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator2 As ToolStripSeparator
+    Friend WithEvents QuitterToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Mini-projet IHM/Form1.vb
@@ -0,0 +1,121 @@
+﻿Public Class Form1
+
+    Private imageName As String
+    Private fichierCsv1 As New FichierCsv
+    Private cheminFichierCsv As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        EnregistrerToolStripMenuItem.Enabled = False
+    End Sub
+
+    Private Sub OuvrirImageToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OuvrirImageToolStripMenuItem.Click
+        OpenFileDialog1.Filter = "Image |*.jpg;*.jpeg;*.png"
+
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            Me.imageName = OpenFileDialog1.FileName
+            OpenFileDialog1.InitialDirectory = OpenFileDialog1.FileName
+            PictureBox1.Image = Image.FromFile(imageName)
+        End If
+    End Sub
+
+    Private Sub PictureBox1_MouseClick(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseClick
+        If PictureBox1.Image IsNot Nothing Then
+            fichierCsv1.Add(New Annotation(e.X.ToString, e.Y.ToString), Me.imageName)
+        End If
+    End Sub
+
+    Private Sub EnregistrerSousToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EnregistrerSousToolStripMenuItem.Click
+        Dim saveFileDialog1 As SaveFileDialog = New SaveFileDialog
+        saveFileDialog1.Filter = "csv files (*.csv)|*.csv"
+        saveFileDialog1.FilterIndex = 1
+        saveFileDialog1.RestoreDirectory = True
+
+        If saveFileDialog1.ShowDialog = DialogResult.OK Then
+            Save(saveFileDialog1.FileName)
+            cheminFichierCsv = saveFileDialog1.FileName
+            EnregistrerToolStripMenuItem.Enabled = True
+        End If
+    End Sub
+
+    Private Sub EnregistrerToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EnregistrerToolStripMenuItem.Click
+        Save(cheminFichierCsv)
+    End Sub
+
+    Private Sub Save(fileName As String)
+        Dim streamCsv As IO.StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(fileName, False)
+        For i = 0 To fichierCsv1.ListFileName.Count - 1
+            streamCsv.Write(fichierCsv1.ListFileName(i) + " | ")
+            For y = 0 To fichierCsv1.ListAnnotation(i).Count - 1
+                streamCsv.Write(fichierCsv1.ListAnnotation(i)(y).XCoord1.ToString + " " + fichierCsv1.ListAnnotation(i)(y).YCoord1.ToString + " | ")
+            Next
+            streamCsv.WriteLine()
+        Next
+        streamCsv.Close()
+    End Sub
+
+End Class
+
+Public Class FichierCsv
+    Private _listAnnotation As New List(Of List(Of Annotation))
+    Private _listFileName As New List(Of String)
+    Public Property ListAnnotation As List(Of List(Of Annotation))
+        Get
+            Return _listAnnotation
+        End Get
+        Set(value As List(Of List(Of Annotation)))
+            _listAnnotation = value
+        End Set
+    End Property
+    Public Property ListFileName As List(Of String)
+        Get
+            Return _listFileName
+        End Get
+        Set(value As List(Of String))
+            _listFileName = value
+        End Set
+    End Property
+
+    Public Sub Add(annotation As Annotation, imageName As String)
+        Dim indexFileName As Integer
+        If Not ListFileName.Contains(imageName) Then
+            ListFileName.Add(imageName)
+            ListAnnotation.Add(New List(Of Annotation))
+            indexFileName = ListAnnotation.Count - 1
+        Else
+            indexFileName = ListFileName.IndexOf(imageName)
+        End If
+        ListAnnotation(indexFileName).Add(annotation)
+    End Sub
+
+    Public Sub Save(cheminCsv As String)
+        Dim outCsv As IO.StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(cheminCsv, False)
+        outCsv.WriteLine("Bonjour")
+        outCsv.WriteLine("Bonjour")
+    End Sub
+End Class
+Public Class Annotation
+    Private xCoord, yCoord As Double
+
+    Public Sub New(xCoord As Double, yCoord As Double)
+        Me.xCoord = xCoord
+        Me.yCoord = yCoord
+    End Sub
+
+    Public Property XCoord1 As Double
+        Get
+            Return xCoord
+        End Get
+        Set(value As Double)
+            xCoord = value
+        End Set
+    End Property
+
+    Public Property YCoord1 As Double
+        Get
+            Return yCoord
+        End Get
+        Set(value As Double)
+            yCoord = value
+        End Set
+    End Property
+End Class
